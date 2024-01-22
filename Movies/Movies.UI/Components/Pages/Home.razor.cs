80bcdba0db4570238a0528ddcb1e92771b64cb96
Oskar Pilarski <oskar.pilarski@piinsight.onmicrosoft.com>
2024-01-22 15:54:42 +0000

    [main] Little refactor of UI, moved stuff into razor.cs class to keep it cleaner, added little loading spinner.

---
+++ Movies/Movies.UI/Components/Pages/Home.razor.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Components;
+using Movies.Models.ViewModels;
+
+namespace Movies.UI.Components.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        IConfiguration _configuration { get; set; }
+        [Inject]
+        HttpClient _httpClient { get; set; }
+
+        List<MovieViewModel> movies = new List<MovieViewModel>();
+        int take = 10;
+        int skip = 0;
+        int currentPage = 1;
+        int selectedTakeValue = 10;
+        bool isLoading = false;
+        string selectedProperty { get; set; } = "Title";
+        string filterValue { get; set; } = "";
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadMovies();
+        }
+
+        private IEnumerable<string> GetMovieViewModelProperties()
+        {
+            var propertyNames = typeof(MovieViewModel).GetProperties()
+                .Select(propertyInfo => propertyInfo.Name);
+
+            return propertyNames;
+        }
+
+        private async Task ApplyFilter()
+        {
+            isLoading = true;
+            var apiUrl = $"{_configuration.GetSection("MovieApiUrl").Value}?Take={take}&Skip={skip}&Filter={filterValue}&FilterByType={selectedProperty}";
+
+            // Call the API to load movies with filters
+            var response = await _httpClient.GetFromJsonAsync<List<MovieViewModel>>(apiUrl);
+
+            if (response != null)
+            {
+                movies = response;
+            }
+            isLoading = false;
+        }
+
+        private async Task LoadMovies()
+        {
+            isLoading = true;
+            var response = await _httpClient.GetFromJsonAsync<List<MovieViewModel>>(
+                $"{_configuration.GetSection("MovieApiUrl").Value}?Take={take}&Skip={skip}");
+
+            if (response != null)
+            {
+                movies = response;
+            }
+            isLoading = false;
+        }
+
+        private async Task LoadNextPage()
+        {
+            skip += take;
+            currentPage++;
+            await LoadMovies();
+        }
+
+        private async Task LoadPreviousPage()
+        {
+            if (skip >= take)
+            {
+                skip -= take;
+                currentPage--;
+                await LoadMovies();
+            }
+        }
+
+        private async Task UpdateTakeValue(ChangeEventArgs e)
+        {
+            selectedTakeValue = int.Parse(e.Value.ToString());
+            take = selectedTakeValue;
+            skip = 0;
+            currentPage = 1;
+            await LoadMovies();
+        }
+    }
+}
